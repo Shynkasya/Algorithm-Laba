18154f9e95aae6295fbecd8afe3336ae4d43a3ad
Lesya <oshynkaren@stud.khpi.ucode-connect.study>
2024-10-07 19:49:32 +0300

    Main

--- Laba2/Laba2/Laba2.cpp
+++ Laba2/Laba2/Laba2.cpp
@@ -1,6 +1,14 @@
 #include"Laba2.h"
 
+
 int main() {
-	stack_menu();
+	queue list;
+	list.begin = NULL;
+	list.end = NULL;
+	element_before_min_queue(list);
+	create_queue(&list, 5);
+	print_queue(list);
+	delete_queue(&list);
+	print_queue(list);
 	return 0;
 }
