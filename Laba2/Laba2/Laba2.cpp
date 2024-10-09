8973687aa26ed51d6edcb39ccb257f93647f4b2a
Lesya <oshynkaren@stud.khpi.ucode-connect.study>
2024-10-09 16:28:25 +0300

    Main

--- Laba2/Laba2/Laba2.cpp
+++ Laba2/Laba2/Laba2.cpp
@@ -1,14 +1,35 @@
 #include"Laba2.h"
 
-
 int main() {
-	queue list;
-	list.begin = NULL;
-	list.end = NULL;
-	element_before_min_queue(list);
-	create_queue(&list, 5);
-	print_queue(list);
-	delete_queue(&list);
-	print_queue(list);
+	bool quit = true;
+	int num_menu;
+	while (quit) {
+		system("cls");
+		cout << "1. Stack\n";
+		cout << "2. Queue\n";
+		cout << "3. Deque\n";
+		cout << "4. Exit\n";
+		cout << "Choose oparetion: ";
+		cin >> num_menu;
+		system("cls");
+		switch (num_menu)
+		{
+		case 1:
+			stack_menu();
+			break;
+		case 2:
+			queue_menu();
+			break;
+		case 3:
+			deque_menu();
+			break;
+		case 4:
+			quit = false;
+			break;
+		default:
+			cout << "Ty daun probui eshche raz\n";
+			break;
+		}
+	}
 	return 0;
-}
+} 
