b3fd76dfe8ac4e158ffb06ccf51d2f82f2213cda
Lesya <oshynkaren@stud.khpi.ucode-connect.study>
2024-10-17 20:46:46 +0300

    First six task

--- Laba3/Laba3/Laba3.cpp
+++ Laba3/Laba3/Laba3.cpp
@@ -1,7 +1,10 @@
 #include<string>
 #include<iostream>
 #include<algorithm>
+#include <time.h>
+
 using namespace std;
+string Surnames[5] = { "f", "a", "z", "g", "p" };
 struct tree_node {
 	//public:	
 	string surname;
@@ -18,12 +21,15 @@ void update_height(tree_node* node) {
 	node->height = max(node->left->height, node->right->height) + 1;
 }
 void enter_tree(tree_node* node) {
-	cout << "Enter name: ";
+	/*cout << "Enter name: ";
 	cin >> node->surname;
 	cout << "Enter private number: ";
 	cin >> node->private_number;
 	cout << "Enter experience: ";
-	cin >> node->experience;
+	cin >> node->experience;*/
+	node->surname = Surnames[rand() % 5];
+	node->private_number = rand() % 100 + 1;
+	node->experience = rand() % 15 + 1;
 	node->left = NULL;
 	node->right = NULL;
 }
@@ -107,10 +113,11 @@ void delete_left_tree(tree_node** tree) {
 	if ((*tree) == NULL) return;
 	delete_tree(&(*tree)->left);
 }
-tree_node* get_max_node(tree_node* tree) {
-	if (tree == NULL) return NULL;
-	if (tree->left == NULL) return tree;
-	get_max_node(tree->left);
+tree_node* get_parent_min_node(tree_node* tree) {
+	//if (tree == NULL) return NULL;
+	//if (tree->right == NULL) return tree;
+	if (tree->right->right == NULL) return tree;
+	get_parent_min_node(tree->right);
 }
 void delete_node(tree_node** node) {
 	if ((*node) == NULL) return;
@@ -127,21 +134,65 @@ void delete_node(tree_node** node) {
 		tmp = NULL;
 		return;
 	}
-	tmp = get_max_node((*node)->left);
+	if ((*node)->left->right == NULL) {
+		tmp = (*node)->left;
+		(*node)->surname = tmp->surname;
+		(*node)->private_number = tmp->private_number;
+		(*node)->experience = tmp->experience;
+		(*node)->left = NULL;
+		return;
+	}
+	tmp = get_parent_min_node((*node)->left);
 	cout << "*******\n";
-	print_node(tmp);
+	print_tree(tmp);
 	cout << "*******\n";
 
-	(*node)->surname = tmp->surname;
-	(*node)->private_number = tmp->private_number;
-	(*node)->experience = tmp->experience;
-	delete_node(&tmp);
+	(*node)->surname = tmp->right->surname;
+	(*node)->private_number = tmp->right->private_number;
+	(*node)->experience = tmp->right->experience;
+	delete(tmp->right);
+	tmp->right = NULL;
+}
+
+void insert_by_exp(tree_node** tree, tree_node* node) {
+	if ((*tree)->experience < node->experience) {
+		if ((*tree)->left == NULL) (*tree)->left = node;
+		else insert_by_exp(&(*tree)->left, node);
+	}
+	if ((*tree)->experience >= node->experience) {
+		if ((*tree)->right == NULL) (*tree)->right = node;
+		else insert_by_exp(&(*tree)->right, node);
+	}
+}
+void make_experience_tree(tree_node** tree, tree_node** root) {
+	tree_node* tmp = NULL;
+	if ((*tree) == NULL) return;
+	make_experience_tree(&(*tree)->right, &(*root));
+	make_experience_tree(&(*tree)->left, &(*root));
+	tmp = new tree_node;
+	tmp->surname = (*tree)->surname;
+	tmp->private_number = (*tree)->private_number;
+	tmp->experience = (*tree)->experience;
+	tmp->left = NULL;
+	tmp->right = NULL;
+	if ((*root) == NULL) (*root) = tmp;
+	else insert_by_exp(*&root, tmp);
+}
+
+tree_node* resort_tree_by_experience(tree_node** tree) {
+	tree_node* changed_tree = NULL;
+	make_experience_tree(&(*tree), &changed_tree);
+	delete_tree(&(*tree));
+	return changed_tree;
 }
 int main() {
+	srand(time(NULL));
 	tree_node *tree = NULL;
 	make_binary_tree(&tree, 4);
-	//print_tree(tree);
-	delete_node(&tree);
+	print_tree(tree);
+	system("pause");
+	cout << "******************\n";
+	tree = resort_tree_by_experience(&tree);
 	print_tree(tree);
 	/*if (tree == NULL) cout << "Tree is Null";
 	delete_tree(&tree);
