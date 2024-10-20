c4d7b3f5c0f4a67bf08744e70ededc8c25661c62
Lesya <oshynkaren@stud.khpi.ucode-connect.study>
2024-10-20 12:20:13 +0300

    Binary tree approximatelly realization

--- Laba3/Laba3/Laba3.cpp
+++ Laba3/Laba3/Laba3.cpp
@@ -4,7 +4,7 @@
 #include <time.h>
 
 using namespace std;
-string Surnames[5] = { "f", "a", "z", "g", "p" };
+string Surnames[26] = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 struct tree_node {
 	//public:	
 	string surname;
@@ -17,9 +17,6 @@ struct tree_node {
 
 	//function
 };
-void update_height(tree_node* node) {
-	node->height = max(node->left->height, node->right->height) + 1;
-}
 void enter_tree(tree_node* node) {
 	/*cout << "Enter name: ";
 	cin >> node->surname;
@@ -27,18 +24,19 @@ void enter_tree(tree_node* node) {
 	cin >> node->private_number;
 	cout << "Enter experience: ";
 	cin >> node->experience;*/
-	node->surname = Surnames[rand() % 5];
+	node->surname = Surnames[rand() % 26];
 	node->private_number = rand() % 100 + 1;
 	node->experience = rand() % 15 + 1;
 	node->left = NULL;
 	node->right = NULL;
 }
 void insert(tree_node** tree, tree_node* node) {
-	if ((*tree)->surname < node->surname) {
+	node->height++;
+	if ((*tree)->surname > node->surname) {
 		if ((*tree)->left == NULL) (*tree)->left = node;
 		else insert(&(*tree)->left, node);
 	}
-	if ((*tree)->surname >= node->surname) {
+	if ((*tree)->surname <= node->surname) {
 		if ((*tree)->right == NULL) (*tree)->right = node;
 		else insert(&(*tree)->right, node);
 	}
@@ -56,17 +54,33 @@ void make_binary_tree(tree_node** root, int n) {
 }
 
 void print_node(tree_node* node) {
-	cout << node->surname << endl;
-	cout << node->private_number << endl;
-	cout << node->experience << endl;
+	cout << "Surname: " << node->surname << endl;
+	cout << "Privat number: " << node->private_number << endl;
+	cout << "Experience: " << node->experience << endl;
 	cout << endl;
 }
-
+void print_structure(tree_node* node) {
+	if (node == NULL) return;
+	print_structure(node->right);
+	for (int i = 0; i < node->height; i++) cout << "    ";
+	cout << "Surname: " << node->surname << endl;
+	for (int i = 0; i < node->height; i++) cout << "    ";
+	cout << "Privat number: " << node->private_number << endl;
+	for (int i = 0; i < node->height; i++) cout << "    ";
+	cout << "Experience: " << node->experience << endl;
+	print_structure(node->left);
+}
 void print_tree(tree_node* node) {
 	if (node == NULL) return;
 	print_tree(node->right);
 	print_node(node);
 	print_tree(node->left);
+}
+void update_height(tree_node** tree, int height = -1) {
+	if ((*tree) == NULL) return;
+	update_height(&(*tree)->right, (*tree)->height);
+	(*tree)->height = height + 1;
+	update_height(&(*tree)->right, (*tree)->height);
 }
 
 void print_preorder_tree(tree_node* node) {
@@ -116,8 +130,8 @@ void delete_left_tree(tree_node** tree) {
 tree_node* get_parent_min_node(tree_node* tree) {
 	//if (tree == NULL) return NULL;
 	//if (tree->right == NULL) return tree;
-	if (tree->right->right == NULL) return tree;
-	get_parent_min_node(tree->right);
+	if (tree->left->left == NULL) return tree;
+	get_parent_min_node(tree->left);
 }
 void delete_node(tree_node** node) {
 	if ((*node) == NULL) return;
@@ -126,40 +140,42 @@ void delete_node(tree_node** node) {
 		(*node) = (*node)->right;
 		delete tmp;
 		tmp = NULL;
+		update_height(&(*node));
 		return;
 	}
 	if ((*node)->right == NULL) {
 		(*node) = (*node)->left;
 		delete tmp;
 		tmp = NULL;
+		update_height(&(*node));
 		return;
 	}
-	if ((*node)->left->right == NULL) {
-		tmp = (*node)->left;
-		(*node)->surname = tmp->surname;
-		(*node)->private_number = tmp->private_number;
-		(*node)->experience = tmp->experience;
-		(*node)->left = NULL;
+	if ((*node)->right->left == NULL) {
+		(*node)->right->left = (*node)->left;
+		tmp = (*node)->right;
+		delete (*node);
+		(*node) = tmp;
+		update_height(&(*node));
 		return;
 	}
-	tmp = get_parent_min_node((*node)->left);
-	cout << "*******\n";
-	print_tree(tmp);
-	cout << "*******\n";
+	tmp = get_parent_min_node((*node)->right);
 
-	(*node)->surname = tmp->right->surname;
-	(*node)->private_number = tmp->right->private_number;
-	(*node)->experience = tmp->right->experience;
-	delete(tmp->right);
-	tmp->right = NULL;
+	(*node)->surname = tmp->left->surname;
+	(*node)->private_number = tmp->left->private_number;
+	(*node)->experience = tmp->left->experience;
+	tree_node* tmp_1 = tmp->left->right;
+	delete(tmp->left);
+	tmp->left = tmp_1;
+	update_height(&(*node));
 }
 
 void insert_by_exp(tree_node** tree, tree_node* node) {
-	if ((*tree)->experience < node->experience) {
+	node->height++;
+	if ((*tree)->experience > node->experience) {
 		if ((*tree)->left == NULL) (*tree)->left = node;
 		else insert_by_exp(&(*tree)->left, node);
 	}
-	if ((*tree)->experience >= node->experience) {
+	if ((*tree)->experience <= node->experience) {
 		if ((*tree)->right == NULL) (*tree)->right = node;
 		else insert_by_exp(&(*tree)->right, node);
 	}
@@ -177,6 +193,7 @@ void make_experience_tree(tree_node** tree, tree_node** root) {
 	tmp->right = NULL;
 	if ((*root) == NULL) (*root) = tmp;
 	else insert_by_exp(*&root, tmp);
+	update_height(&(*tree));
 }
 
 tree_node* resort_tree_by_experience(tree_node** tree) {
@@ -185,15 +202,202 @@ tree_node* resort_tree_by_experience(tree_node** tree) {
 	delete_tree(&(*tree));
 	return changed_tree;
 }
+
+
+
+
+
+
+enum color { RED, BLACK };
+struct rb_tree{
+	string surname;
+	int private_number;
+	int experience = 0;
+	int height = 0;
+	color color = BLACK;
+	rb_tree* right;
+	rb_tree* left;
+	rb_tree* parent;
+};
+void rotate_right(rb_tree** node) {
+	if ((*node)->parent == NULL) {
+		rb_tree* tmp = (*node)->left;
+		(*node)->left = tmp->right;
+		tmp->right = (*node);
+		tmp->parent = NULL;
+		(*node)->parent = tmp;
+		(*node)->left->parent = (*node);
+		//(*node) = tmp;
+		return;
+	}
+	rb_tree* tmp = (*node)->parent;
+	tmp->right = (*node)->left;
+	(*node)->left = tmp->right->right;
+	tmp->right->right = (*node);
+	tmp->right->parent = tmp;
+	(*node)->parent = tmp->right;
+	(*node)->left->parent = (*node);
+	//(*node) = tmp->right;
+}
+void rotate_left(rb_tree** node) {
+	if ((*node)->parent == NULL) {
+		rb_tree* tmp = (*node)->right;
+		(*node)->right = tmp->left;
+		tmp->left = (*node);
+		tmp->parent = NULL;
+		(*node)->parent = tmp;
+		(*node)->right->parent = (*node);
+		//(*node) = tmp;
+		return;
+	}
+	rb_tree* tmp = (*node)->parent;
+	tmp->left = (*node)->right;
+	(*node)->right = tmp->left->left;
+	tmp->left->left = (*node);
+	tmp->left->parent = tmp;
+	(*node)->parent = tmp->left;
+	(*node)->right->parent = (*node);
+	//(*node) = tmp->left;
+}
+color return_color(rb_tree* node) {
+	if (node == NULL) return BLACK;
+	return node->color;
+}
+void balance_tree(rb_tree** node) {
+	rb_tree* uncle;
+	bool is_left_balance = false;
+	while (return_color((*node)->parent) == RED) {
+		uncle = (*node)->parent->parent->left;
+		is_left_balance = false;
+		if (uncle->surname ==  (*node)->parent->surname) {
+			uncle = (*node)->parent->parent->right;
+			is_left_balance = true;
+		}
+		if (return_color(uncle) == RED) {
+			uncle->color = BLACK;
+			(*node)->parent->color = BLACK;
+			if (uncle->parent->parent != NULL) uncle->parent->color == RED;
+			(*node) = (*node)->parent->parent;
+		}
+		else {
+			if (is_left_balance) {
+				if ((*node)->surname == (*node)->parent->right->surname) {
+					(*node) = (*node)->parent;
+					rotate_left(*&node);
+				}
+				(*node)->parent->color = BLACK;
+				(*node)->parent->parent->color = RED;
+				rotate_right(&(*node)->parent->parent);
+			}
+			else {
+				if ((*node)->surname == (*node)->parent->left->surname) {
+					(*node) = (*node)->parent;
+					rotate_right(*&node);
+				}
+				(*node)->parent->color = BLACK;
+				(*node)->parent->parent->color = RED;
+				rotate_left(&(*node)->parent->parent);
+			}
+		}
+	}
+}
+void update_rb_height(rb_tree** tree, int height = -1) {
+	if ((*tree) == NULL) return;
+	update_rb_height(&(*tree)->right, (*tree)->height);
+	(*tree)->height = height + 1;
+	update_rb_height(&(*tree)->right, (*tree)->height);
+}
+void insert_rb_node(rb_tree** tree, rb_tree* node) {
+	if ((*tree)->surname < node->surname) {
+		if ((*tree)->left == NULL) {
+			(*tree)->left = node;
+			node->parent = (*tree);
+		}
+		else insert_rb_node(&(*tree)->left, node);
+	}
+	if ((*tree)->surname >= node->surname) {
+		if ((*tree)->right == NULL) {
+			(*tree)->right = node;
+			node->parent = (*tree);
+		}
+		else insert_rb_node(&(*tree)->right, node);
+	}
+	balance_tree(&(*tree));
+	update_rb_height(&(*tree));
+}
+void enter_rb_tree(rb_tree* node) {
+	/*cout << "Enter name: ";
+	cin >> node->surname;
+	cout << "Enter private n  umber: ";
+	cin >> node->private_number;
+	cout << "Enter experience: ";
+	cin >> node->experience;*/
+	node->surname = Surnames[rand() % 26];
+	node->private_number = rand() % 100 + 1;
+	node->experience = rand() % 15 + 1;
+	node->color = RED;
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = NULL;
+}
+void colorize_tree(rb_tree** root, int n) {
+	rb_tree* tmp;
+	while (n > 0) {
+		tmp = new rb_tree;
+		enter_rb_tree(tmp);
+		if ((*root) == NULL) { 
+			(*root) = tmp; 
+			(*root)->color = BLACK;
+			(*root)->surname = "z";
+		}
+		else insert_rb_node(*&root, tmp);
+		cout << "Element is insert\n";
+		n--;
+	}
+}
+void print_rb_node(rb_tree* node) {
+	for (int i = 0; i < node->height; i++) cout << "    ";
+	cout << node->surname << endl;
+	for (int i = 0; i < node->height; i++) cout << "    ";
+	cout << node->private_number << endl;
+	for (int i = 0; i < node->height; i++) cout << "    ";
+	cout << node->experience << endl;
+	cout << endl;
+}
+void print_rb_tree(rb_tree* tree){
+	/*if (tree == NULL) return;
+	print_rb_tree(tree->right);
+	print_rb_node(tree);
+	print_rb_tree(tree->left);*/
+	if (tree == NULL) return;
+	print_rb_tree(tree->right);
+	print_rb_node(tree);
+	print_rb_tree(tree->left);
+}
+
 int main() {
 	srand(time(NULL));
-	tree_node *tree = NULL;
-	make_binary_tree(&tree, 4);
-	print_tree(tree);
-	system("pause");
-	cout << "******************\n";
-	tree = resort_tree_by_experience(&tree);
-	print_tree(tree);
+	
+	rb_tree* tree = NULL;
+	colorize_tree(&tree, 8);
+	print_rb_tree(tree);
+	rotate_left(&tree);
+	cout << "*********************";
+	print_rb_tree(tree);
+
+
+	//tree_node *tree = NULL;
+	//make_binary_tree(&tree, 11);
+	//print_structure(tree);
+	//system("pause");
+	//cout << "******************\n";
+	////tree = resort_tree_by_experience(&tree);
+	//delete_node(&tree);
+	//print_structure(tree);
+	
+	
+	
+	
 	/*if (tree == NULL) cout << "Tree is Null";
 	delete_tree(&tree);
 	if (tree == NULL) cout << "Tree is Null";
