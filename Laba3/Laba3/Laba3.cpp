cceed13cc0fe82b4bbb01dc8f235676f727916ef
Lesya <oshynkaren@stud.khpi.ucode-connect.study>
2024-10-20 21:28:08 +0300

    Wrong balance function

--- Laba3/Laba3/Laba3.cpp
+++ Laba3/Laba3/Laba3.cpp
@@ -219,14 +219,15 @@ struct rb_tree{
 	rb_tree* left;
 	rb_tree* parent;
 };
-void rotate_right(rb_tree** node) {
+void rotate_right(rb_tree** node, rb_tree** root) {
 	if ((*node)->parent == NULL) {
 		rb_tree* tmp = (*node)->left;
 		(*node)->left = tmp->right;
 		tmp->right = (*node);
-		tmp->parent = NULL;
 		(*node)->parent = tmp;
-		(*node)->left->parent = (*node);
+		if ((*node)->left != NULL) (*node)->left->parent = (*node);
+		tmp->parent = NULL;
+		(*root) = tmp;
 		//(*node) = tmp;
 		return;
 	}
@@ -236,17 +237,18 @@ void rotate_right(rb_tree** node) {
 	tmp->right->right = (*node);
 	tmp->right->parent = tmp;
 	(*node)->parent = tmp->right;
-	(*node)->left->parent = (*node);
+	if((*node)->left != NULL) (*node)->left->parent = (*node);
 	//(*node) = tmp->right;
 }
-void rotate_left(rb_tree** node) {
+void rotate_left(rb_tree** node, rb_tree ** root) {
 	if ((*node)->parent == NULL) {
 		rb_tree* tmp = (*node)->right;
 		(*node)->right = tmp->left;
 		tmp->left = (*node);
-		tmp->parent = NULL;
 		(*node)->parent = tmp;
-		(*node)->right->parent = (*node);
+		if ((*node)->right != NULL)(*node)->right->parent = (*node);
+		tmp->parent = NULL;
+		(*root) = tmp;
 		//(*node) = tmp;
 		return;
 	}
@@ -256,47 +258,47 @@ void rotate_left(rb_tree** node) {
 	tmp->left->left = (*node);
 	tmp->left->parent = tmp;
 	(*node)->parent = tmp->left;
-	(*node)->right->parent = (*node);
+	if((*node)->right != NULL) (*node)->right->parent = (*node);
 	//(*node) = tmp->left;
 }
 color return_color(rb_tree* node) {
 	if (node == NULL) return BLACK;
 	return node->color;
 }
-void balance_tree(rb_tree** node) {
+void balance_tree(rb_tree* node, rb_tree** root) {
 	rb_tree* uncle;
 	bool is_left_balance = false;
-	while (return_color((*node)->parent) == RED) {
-		uncle = (*node)->parent->parent->left;
+	while (return_color(node->parent) == RED) {
+		uncle = node->parent->parent->left;
 		is_left_balance = false;
-		if (uncle->surname ==  (*node)->parent->surname) {
-			uncle = (*node)->parent->parent->right;
+		if (uncle == node->parent) {
+			uncle = node->parent->parent->right;
 			is_left_balance = true;
 		}
 		if (return_color(uncle) == RED) {
 			uncle->color = BLACK;
-			(*node)->parent->color = BLACK;
-			if (uncle->parent->parent != NULL) uncle->parent->color == RED;
-			(*node) = (*node)->parent->parent;
+			node->parent->color = BLACK;
+			if (uncle->parent->parent != NULL) uncle->parent->color = RED;
+			node = node->parent->parent;
 		}
 		else {
 			if (is_left_balance) {
-				if ((*node)->surname == (*node)->parent->right->surname) {
-					(*node) = (*node)->parent;
-					rotate_left(*&node);
+				if (node == node->parent->right) {
+					node = node->parent;
+					rotate_left(&node, *&root);
 				}
-				(*node)->parent->color = BLACK;
-				(*node)->parent->parent->color = RED;
-				rotate_right(&(*node)->parent->parent);
+				node->parent->color = BLACK;
+				node->parent->parent->color = RED;
+				rotate_right(&node->parent->parent, *&root);
 			}
 			else {
-				if ((*node)->surname == (*node)->parent->left->surname) {
-					(*node) = (*node)->parent;
-					rotate_right(*&node);
+				if (node == node->parent->left) {
+					node = node->parent;
+					rotate_right(&node, *&root);
 				}
-				(*node)->parent->color = BLACK;
-				(*node)->parent->parent->color = RED;
-				rotate_left(&(*node)->parent->parent);
+				node->parent->color = BLACK;
+				node->parent->parent->color = RED;
+				rotate_left(&node->parent->parent, *&root);
 			}
 		}
 	}
@@ -312,18 +314,24 @@ void insert_rb_node(rb_tree** tree, rb_tree* node) {
 		if ((*tree)->left == NULL) {
 			(*tree)->left = node;
 			node->parent = (*tree);
+			//balance_tree(&(*tree));
+			//update_rb_height(&(*tree));
+			//balance_tree(node, *&tree);
+			//return;
 		}
 		else insert_rb_node(&(*tree)->left, node);
 	}
-	if ((*tree)->surname >= node->surname) {
+	else{
 		if ((*tree)->right == NULL) {
 			(*tree)->right = node;
 			node->parent = (*tree);
+			/*balance_tree(&(*tree));
+			update_rb_height(&(*tree));*/
+			//balance_tree(node, *&tree);
+			//return;
 		}
 		else insert_rb_node(&(*tree)->right, node);
 	}
-	balance_tree(&(*tree));
-	update_rb_height(&(*tree));
 }
 void enter_rb_tree(rb_tree* node) {
 	/*cout << "Enter name: ";
@@ -348,7 +356,7 @@ void colorize_tree(rb_tree** root, int n) {
 		if ((*root) == NULL) { 
 			(*root) = tmp; 
 			(*root)->color = BLACK;
-			(*root)->surname = "z";
+			(*root)->surname = "a";
 		}
 		else insert_rb_node(*&root, tmp);
 		cout << "Element is insert\n";
@@ -381,9 +389,9 @@ int main() {
 	rb_tree* tree = NULL;
 	colorize_tree(&tree, 8);
 	print_rb_tree(tree);
-	rotate_left(&tree);
+	/*rotate_left(&tree, );
 	cout << "*********************";
-	print_rb_tree(tree);
+	print_rb_tree(tree);*/
 
 
 	//tree_node *tree = NULL;
