2e2f3d17a9ec8b778b31221b8941f9930c036c5c
Lesya <oshynkaren@stud.khpi.ucode-connect.study>
2024-10-28 23:25:39 +0200

    Header

---
+++ Laba4/Laba4/Laba4.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cmath>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+
+#define A (sqrt(5) - 1) / 2
+
+using namespace std;
+
+
+void task1_menu();
+void task2_menu();
+void task3_help_menu();
