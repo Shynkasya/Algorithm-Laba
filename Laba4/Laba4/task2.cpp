5edc9d571cfc843ab7b43fd2e4bed99ce2276ff7
Lesya <oshynkaren@stud.khpi.ucode-connect.study>
2024-11-01 09:32:39 +0200

    Task2 with search

--- Laba4/Laba4/task2.cpp
+++ Laba4/Laba4/task2.cpp
@@ -42,6 +42,25 @@ void print_hash_table_2(hash_node_2* hash_table, int size) {
 		}
 	}
 }
+int search_node_2(hash_node_2* hash_table, int key, int size) {
+	int h = hash_function_2(key, size);
+	if (hash_table[h].key == key) {
+		return h;
+	}
+	else {
+		hash_node_2* ptr;
+		ptr = &hash_table[h];
+		int counter = 0;
+		while (ptr != NULL && ptr->key != key) {
+			ptr = ptr->next;
+			counter++;
+		}
+		if (ptr == NULL) { 
+			cout << "Table haven't got such element\n";
+			return -1; 
+		} else return counter;
+	}
+}
 void delete_node_2(hash_node_2* hash_table, int key, int size) {
 	int h = hash_function_2(key, size);
 	hash_node_2* ptr;
@@ -108,6 +127,7 @@ void task2_menu() {
 	int arr[] = { 1, 99, 33, 5, 3, 37, 44, 32, 12 };
 	hash_node_2* hash_table = new hash_node_2[9];
 	int m = 9;
+	int h;
 	bool quit = true;
 	int num_menu;
 	while (quit) {
@@ -118,7 +138,8 @@ void task2_menu() {
 		cout << "4. Write hash table to file\n";
 		cout << "5. Add node\n";
 		cout << "6. Delete node\n";
-		cout << "7. Exit\n";
+		cout << "7. Search node\n";
+		cout << "8. Exit\n";
 		cout << "Choose oparetion: ";
 		cin >> num_menu;
 		switch (num_menu)
@@ -154,6 +175,15 @@ void task2_menu() {
 			delete_node_2(*&hash_table, num_menu, m);
 			break;
 		case 7:
+			cout << "Enter key of element: ";
+			cin >> num_menu;
+			h = hash_function_2(num_menu, m);
+			num_menu = search_node_2(hash_table, num_menu, m);
+			if (num_menu != -1) {
+				cout << "Your element is on " << h << " position on table and in " << num_menu << " position on list\n";
+			}
+			break;
+		case 8:
 			quit = false;
 			break;
 		default:
